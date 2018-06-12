42526529485c87b0455993a6311489f3efb43e62
beewhoo <bibeksinghthapa@gmail.com>
2018-06-12 13:38:25 -0400

    completed

---
+++ exercise.rb
@@ -0,0 +1,24 @@
+
+# en = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+#
+
+
+digits = ['1','2','3','4','5','6','7','8','9']
+en = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+fr = ['un','deux','trois','quatre','cinq','six','sept','huit','neuf']
+
+
+
+
+
+
+
+
+hash = {}
+
+digits.each  do |num|
+  hash[num] = {french: fr[num.to_i - 1], english: en[num.to_i - 1]}
+end
+
+p hash
